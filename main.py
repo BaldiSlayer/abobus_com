79fe5722a47bf4c94d9742088236bdad76376eb8
Dhhdhdjsjssjsj <77489413+Dhhdhdjsjssjsj@users.noreply.github.com>
2021-11-04 12:33:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import random, string
+from flask import Flask, render_template, request, jsonify, make_response, redirect
+
+online_users = {}
+
+def generate_random_string(length):
+    return ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + '0123456789') for i in range(length))
+
+def auth(l, p):
+    if l == 'admin' and p == 'admin':
+        return True
+
+    return False
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/account/<token>")
+def about(token):
+    if token in online_users:
+        return render_template('managet_account.html', login=online_users[token][0])
+    return 'Ошибка доступа'
+    
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+@app.route('/login', methods=['POST'])
+def hello():
+    login = request.form['login']
+    password = request.form['password']
+
+    global online_users
+
+    if auth(login, password):
+        h = generate_random_string(64)
+        online_users[h] = [login, password]
+        return {'success': True, 'token': h}
+    else:
+        return redirect("/")
+if __name__ == "__main__":
+    app.run(debug=True)
