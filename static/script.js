cbfe1d117d9e2c8b9ddcaf6a370ffd907eb78a68
Alexey <77489413+Dhhdhdjsjssjsj@users.noreply.github.com>
2022-09-25 18:06:11 +0300

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function push() {
-    console.log(5555555);
-
-    fetch('/sms', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: {'n': 'b', 'sd': 'sadasda'}
-    });
-}
